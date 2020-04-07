a1f44dd1bb7849342b62f1fabf4a8774095f2638
mxgoddard <maxolivergoddard@gmail.com>
2020-04-07 15:46:17 +0100

    Setup basic navigation bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import './app.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      <h1>Hello, World!</h1>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="App-header">
+                <div class="topnav">
+                    <a href="#temp1">Blank1</a>
+                    <a href="#temp2">Blank2</a>
+                    <a href="#temp3">Blank3</a>
+                    <a href="#temp4">Blank4</a>
+                    <a href="#temp5">Blank5</a>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
