7afe6bc097798c880fc6cb32ae17ffb30dfdb93a
mxgoddard <maxolivergoddard@gmail.com>
2020-09-10 19:05:49 +0100

    Updates

---
+++ precache-manifest.bf5925450c3f90f0737f873bfea58423.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "234257b2e5228cb165818500f05e8299",
+    "url": "/max-site/index.html"
+  },
+  {
+    "revision": "fac5a31142b1c26a4275",
+    "url": "/max-site/static/css/main.7bab3d8a.chunk.css"
+  },
+  {
+    "revision": "4c2cdef1e020b57e631c",
+    "url": "/max-site/static/js/2.f437e05f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/max-site/static/js/2.f437e05f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fac5a31142b1c26a4275",
+    "url": "/max-site/static/js/main.ed005a91.chunk.js"
+  },
+  {
+    "revision": "35a4fbe8e4fb6fdb54a5",
+    "url": "/max-site/static/js/runtime-main.e12ed60e.js"
+  }
+]);
