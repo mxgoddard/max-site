8bbd92cd8c994a52f521a6e47433dd82cf2e76cd
mxgoddard <maxolivergoddard@gmail.com>
2020-09-16 19:01:02 +0100

    Unignore build folder

---
+++ build/precache-manifest.7e1122cbcc034f4e302ecc197ee733ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2242007503a0cd72d5e26e82fec54737",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61c8f571477c7e705834",
+    "url": "/static/css/main.9981f087.chunk.css"
+  },
+  {
+    "revision": "eaa3668cc27869f29667",
+    "url": "/static/js/2.1dd2f306.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1dd2f306.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61c8f571477c7e705834",
+    "url": "/static/js/main.5876db66.chunk.js"
+  },
+  {
+    "revision": "72a7a01541d938eb13e5",
+    "url": "/static/js/runtime-main.7da483d5.js"
+  }
+]);
